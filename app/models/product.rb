cd30a52984c8cb240def7372f8253a239782341d
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-21 11:56:35 +0000

    Create validation for the Product.name in Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
