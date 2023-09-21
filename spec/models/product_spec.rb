daa7174276f8cc4039907294d8c56375ba3be4a5
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-21 15:07:55 +0000

    Create tests for Product.image_url to ensure presence and the correct behaviour.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -105,10 +105,27 @@ RSpec.describe Product, type: :model do
         expect(product.price).to eq(4.99)
       end
     end
-
   end
 
+  context 'validates product.image_url' do
+    describe 'when product.image_url is nil' do
+      let(:product) { Product.new(code: 'SR1', name: 'Strawberries', price: 4.99, image_url: nil) }
 
+      it 'not valid' do
+        expect(product).not_to be_valid
+      end
+    end
 
+    describe 'when product.image_url is present' do
+      let(:product) { Product.new(code: 'SR1', name: 'Strawberries', price: 4.99, image_url: 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')}
 
+      it 'valid' do
+        expect(product).to be_valid
+      end
+
+      it 'sets the correct product.image_url' do
+        expect(product.image_url).to eq('https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')
+      end
+    end
+  end
 end
