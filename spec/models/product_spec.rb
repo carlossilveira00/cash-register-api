c37ecf0e77d61d8e0c291ce966ea71f511da6613
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-21 20:55:24 +0000

    Change quotation marks to ensure equality in the tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Product, type: :model do
       end
     end
 
-    describe "when product.code is blank" do
+    describe 'when product.code is blank' do
       let(:product) { Product.new(code: '', name: 'Strawberries', price: 5.00, image_url: 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')}
 
       it 'not valid' do
@@ -87,7 +87,7 @@ RSpec.describe Product, type: :model do
     end
 
     describe 'when product.price is not a float' do
-      let(:product) { Product.new(code: 'SR1', name: 'Strawberries', price: "2 dollars", image_url: 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')}
+      let(:product) { Product.new(code: 'SR1', name: 'Strawberries', price: '2 dollars', image_url: 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')}
 
       it 'not valid' do
         expect(product).not_to be_valid
