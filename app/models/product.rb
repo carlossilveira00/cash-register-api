4f6096439b358aa72c541f8aa6a271e54042adda
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-21 15:08:55 +0000

    Add validation to the Product Model to ensure the presence of image_url.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :price, presence: true, numericality: { only_float: true }
+  validates :image_url, presence: true
 end
