011462d3e8929b99de4084f39b92b108d6771f51
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-24 21:52:54 +0000

    Create migration to add total column to Cart table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_212642) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_215129) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "product_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_212642) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "total"
   end
 
   create_table "products", force: :cascade do |t|
