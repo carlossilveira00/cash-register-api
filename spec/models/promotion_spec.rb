39adae18cddeb1e2c1a0f8a0c1b519c768f996a3
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-25 14:05:11 +0000

    Create validation tests for Promotion.name to ensure presence, correct type and expected behaviour.

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Promotion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validates promotion.title' do
+    describe 'when promotion.title is nil' do
+      let(:promotion) { FactoryBot.build(:promotion, title: nil) }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.title is not a string' do
+      let(:promotion) { FactoryBot.build(:promotion, title: 5.99) }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.title is blank' do
+      let(:promotion) { FactoryBot.build(:promotion, title: '') }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.title is a string' do
+      let(:promotion) { FactoryBot.build(:promotion) }
+
+      it 'valid' do
+        expect(promotion).to be_valid
+      end
+
+      it 'sets the correct promotion.title' do
+        expect(promotion.title).to eq('Green Tea Promotion')
+      end
+    end
+  end
+
 end
