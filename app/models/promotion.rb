39660e1d95679747869cc4af84a0863b58a82530
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-25 14:12:00 +0000

    Add validation in Promotion model to ensure product_code presence and correct format.

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,4 @@
 class Promotion < ApplicationRecord
   validates :title, presence: true, format: { with: /\A[\w\s\d]+\z/ }
+  validates :product_code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
