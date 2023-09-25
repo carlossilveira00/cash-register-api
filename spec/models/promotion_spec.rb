1fd18568d851d22996e2a05f5576df2216a25db3
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-25 14:10:56 +0000

    Create tests for Promotion.product_code to ensure presence, correct type and expecteed behaviour.

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -39,4 +39,42 @@ RSpec.describe Promotion, type: :model do
     end
   end
 
+  context 'validates promotion.product_code' do
+    describe 'when promotion.product_code is nil' do
+      let(:promotion) { FactoryBot.build(:promotion, product_code: nil) }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.product_code is not a string' do
+      let(:promotion) { FactoryBot.build(:promotion, product_code: 5.99) }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.product_code is blank' do
+      let(:promotion) { FactoryBot.build(:promotion, product_code: '') }
+
+      it 'not valid' do
+        expect(promotion).not_to be_valid
+      end
+    end
+
+    describe 'when promotion.product_code is a string' do
+      let(:promotion) { FactoryBot.build(:promotion) }
+
+      it 'valid' do
+        expect(promotion).to be_valid
+      end
+
+      it 'sets the correct promotion.product_code' do
+        expect(promotion.product_code).to eq('GR1')
+      end
+    end
+  end
+
 end
