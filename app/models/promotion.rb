12e06dfa94b43bdccc28d9affdda39c5382f2530
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-25 14:34:39 +0000

    Create validation for Promotion.type to ensure presence and the type can only be set to a the allowed types of promotions.

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,6 @@
 class Promotion < ApplicationRecord
   validates :title, presence: true, format: { with: /\A[\w\s\d]+\z/ }
   validates :product_code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :type, presence: true
+  validates :type, inclusion: { in: %w[buy_x_get_x_free price_discount_per_quantity percentage_discount_per_quantity], message: 'must be one of type buy_x_get_x_free, price_discount_per_quantity or percentage_discount_per_quantity.' }
 end
