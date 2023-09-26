bd776b434595659655d0b57c463b269999b11426
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-26 12:14:15 +0000

    Create carts#create method in carts controller. Define route for carts#create in routes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,11 @@
 class CartsController < ApplicationController
+  def create
+    @cart = Cart.new(total: 0)
+
+    if @cart.save
+      render json: @cart, status: :ok
+    else
+      render json: { error: 'Your cart cannot be created. Try again later.' }, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/products', to: 'products#index'
+  post '/cart', to: 'carts#create'
 end
