c021f44792f37b2e344f0a4493450397b23e0465
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-26 15:19:42 +0000

    Create tests for Cart#checkout controller, this method takes an object and validates the cart_items as well as the total value of the cart. If everything is correct then it returns a ok status code and a message saying it was successfully.

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe CartsController, type: :controller do
+  let(:cart) do
+    Cart.create(total: 0)
+  end
+
+  let(:product) do
+    Product.create(code: 'GR1', name: 'Green Tea', price: 3.11, image_url: 'www.google.pt')
+  end
+
+  let(:promotion) do
+    Promotion.create(title: 'Green Tea Promotion', product_code: 'GR1', promotion_type: 'buy_x_get_x_free', discount: nil, min_quantity: 1, promotion_free_quantity: 1)
+  end
+
+  let(:request_data) do
+    {
+      cart: {
+        total: 3.11,
+        cart_items: [
+          {
+            cart_id: cart.id,
+            product_id: product.id,
+            quantity: 1,
+            undiscounted_price: 6.12,
+            discounted_price: 3.11,
+            free_quantity: 1,
+            promotion_id: promotion.id,
+            promotion_status: 'applied'
+          }
+        ]
+      }
+    }
+  end
+
   describe 'POST /create' do
     context 'when cart creation is successful' do
       it 'creates a new cart' do
@@ -9,6 +41,20 @@ RSpec.describe CartsController, type: :controller do
 
         expect(response).to have_http_status(:ok)
         expect(created_cart).to be_present
+      end
+    end
+  end
+
+  describe 'checkout' do
+    context 'when all promotion were applied correctly' do
+      it 'returns 200 OK status code' do
+        post :checkout, params: request_data
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns a message stating checkout successfully.' do
+        post :checkout, params: request_data
+        expect(parsed_response['message']).to eq('Checkout completed successfully.')
       end
     end
   end
