9a107b829e67a861135f07bb92d1fc1264ba8f17
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-28 17:43:57 +0000

    Updated title validation on promotion to be able to create titles with symbols and numbers.

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,5 @@
 class Promotion < ApplicationRecord
-  validates :title, presence: true, format: { with: /\A[\w\s\d]+\z/ }
+  validates :title, presence: true
   validates :product_code, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :promotion_type, presence: true
   validates :promotion_type, inclusion: { in: %w[buy_x_get_x_free price_discount_per_quantity percentage_discount_per_quantity], message: 'must be one of type buy_x_get_x_free, price_discount_per_quantity or percentage_discount_per_quantity.' }
